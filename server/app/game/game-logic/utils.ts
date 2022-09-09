d27d2cc0a310de6283e0a280900711267109871d
XavierDupuis <xavier0978@hotmail.fr>
2022-09-09 09:35:17 -0400

    fix OnlineGameSettingsUI verfication utils

--- server/app/game/game-logic/utils.ts
+++ server/app/game/game-logic/utils.ts
@@ -44,15 +44,16 @@ export const getRandomInt = (max: number, min: number = 0): number => {
 
 export const isGameSettings = (obj: unknown): obj is OnlineGameSettingsUI => {
     return (
-        typeof obj === 'object' &&
-        obj !== null &&
-        'gameMode' in obj &&
-        'timePerTurn' in obj &&
-        'playerNames' in obj &&
-        'randomBonus' in obj &&
-        'dictTitle' in obj &&
-        'botDifficulty' in obj &&
-        'numberOfPlayers' in obj
+        (obj as OnlineGameSettingsUI).playerNames !== undefined &&
+        typeof (obj as OnlineGameSettingsUI).playerNames === 'object' &&
+        (obj as OnlineGameSettingsUI).numberOfPlayers !== undefined &&
+        typeof (obj as OnlineGameSettingsUI).numberOfPlayers === 'number' &&
+        (obj as OnlineGameSettingsUI).randomBonus !== undefined &&
+        typeof (obj as OnlineGameSettingsUI).randomBonus === 'boolean' &&
+        (obj as OnlineGameSettingsUI).randomBonus !== undefined &&
+        typeof (obj as OnlineGameSettingsUI).randomBonus === 'boolean' &&
+        (obj as OnlineGameSettingsUI).timePerTurn !== undefined &&
+        typeof (obj as OnlineGameSettingsUI).timePerTurn === 'number'
     );
 };
 
