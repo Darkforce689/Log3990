27ff47d8086ddda22ae14990173ea7890627811a
XavierDupuis <xavier0978@hotmail.fr>
2022-09-18 16:49:22 -0400

    remove unused import

--- mobile/app/src/main/java/com/example/polyscrabbleclient/MainActivity.kt
+++ mobile/app/src/main/java/com/example/polyscrabbleclient/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.polyscrabbleclient.game.components.LetterRack
 import androidx.lifecycle.viewmodel.compose.viewModel
