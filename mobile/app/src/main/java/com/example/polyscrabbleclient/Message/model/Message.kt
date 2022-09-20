877887eb481a17612033140e5385988be6e94124
XavierDupuis <xavier0978@hotmail.fr>
2022-09-20 08:00:52 -0400

    remove Message package

--- mobile/app/src/main/java/com/example/polyscrabbleclient/Message/model/Message.kt
+++
@@ -1,4 +0,0 @@
-package com.example.polyscrabbleclient.message.model
-
-enum class MessageType {SYSTEM, ME, OTHER}
-data class Message(val content: String, val from: String, val type: MessageType)
--- mobile/app/src/main/java/com/example/polyscrabbleclient/Message/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.polyscrabbleclient.message.model
-
-data class UserData(var name:String)
-
-// TODO : Refactor with Auth
-object User {
-    var name = "Proto-type"
-}
