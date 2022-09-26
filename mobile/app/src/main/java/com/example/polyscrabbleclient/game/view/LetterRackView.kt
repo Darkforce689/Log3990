0f23cf66bb7b4bad5d1d86b9e4a868e679e70486
XavierDupuis <xavier0978@hotmail.fr>
2022-09-25 20:37:30 -0400

    letter rack letters lateinit

--- mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/LetterRackView.kt
+++ mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/LetterRackView.kt
@@ -2,7 +2,6 @@ package com.example.polyscrabbleclient.game.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
@@ -10,11 +9,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.polyscrabbleclient.game.sources.Player
 import com.example.polyscrabbleclient.game.viewmodels.LetterRackViewModel
 
 @Composable
-fun LetterRackView(navController: NavController) {
+fun LetterRackView(navController: NavController, user: Player) {
     val viewModel: LetterRackViewModel = viewModel()
+    viewModel.updateLetters(user.letters)
 //    val selectedTiles = remember {
 //        mutableStateMapOf<TileModel, Boolean>()
 //    }
@@ -31,7 +32,6 @@ fun LetterRackView(navController: NavController) {
 
     LazyRow(
         modifier = Modifier
-            .fillMaxSize()
             .background(MaterialTheme.colors.background),
         horizontalArrangement = Arrangement.SpaceAround,
         verticalAlignment = Alignment.Bottom
--- mobile/app/src/main/java/com/example/polyscrabbleclient/game/viewmodels/LetterRackViewModel.kt
+++ mobile/app/src/main/java/com/example/polyscrabbleclient/game/viewmodels/LetterRackViewModel.kt
@@ -1,14 +1,16 @@
 package com.example.polyscrabbleclient.game.viewmodels
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
-import com.example.polyscrabbleclient.game.domain.LetterCreator
-import com.example.polyscrabbleclient.game.sources.Player
+import com.example.polyscrabbleclient.game.domain.TileCreator
+import com.example.polyscrabbleclient.game.model.TileModel
 
 
+class LetterRackViewModel : ViewModel()
+{
+    private val tileCreator = TileCreator();
+    lateinit var tiles:List<TileModel>
 
-class LetterRackViewModel : ViewModel() {
-    private val letterCreator = LetterCreator();
-    private val player = Player()
-    val tiles = player.letters.map { letter -> letterCreator.createTileFromLetter(letter) }
+    fun updateLetters(letters: List<Char>) {
+        tiles = letters.map { letter -> tileCreator.createTileFromLetter(letter) }
+    }
 }
