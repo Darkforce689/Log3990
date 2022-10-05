a7cdb0b365f89c5a6c475c25d4c60fd1be2ad3a7
XavierDupuis <xavier0978@hotmail.fr>
2022-10-05 08:52:43 -0400

    canvas grid view wip

--- mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/BoardView.kt
+++ mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/BoardView.kt
@@ -1,31 +1,58 @@
 package com.example.polyscrabbleclient.game.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.Text
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.polyscrabbleclient.game.domain.TileCreator
-import com.example.polyscrabbleclient.game.model.RowChar
-import com.example.polyscrabbleclient.game.model.TileModel
+import com.example.polyscrabbleclient.game.model.BoardDimension
 import com.example.polyscrabbleclient.game.viewmodels.BoardViewModel
+
+const val GridDimension = BoardDimension + 1
+// TODO : CHANGE DP <-> Float
+const val GridSize = 740f
+const val GridDivisionSize = GridSize / GridDimension
 
 @Composable
 fun BoardView () {
     val viewModel: BoardViewModel = viewModel()
 
-    viewModel.board[1, RowChar.A] = TileCreator().createTileFromLetter('A')
-    Row {
-        for(rows in viewModel.board.tileGrid) {
-            Column {
-                for(tileContainer in rows) {
-                   val letter = tileContainer.value?.letter ?: '-'
-                   Text(text = letter.toString())
-                }
-            }
+    val themeColors = listOf(MaterialTheme.colors.primary, MaterialTheme.colors.secondary, MaterialTheme.colors.secondary)
+    val thickDividerIndexes = listOf(0,1,GridDimension);
+
+    Canvas(
+        modifier = Modifier
+            // TODO : CHANGE DP <-> Float
+            .size(300.dp)
+            .padding(16.dp)
+    ) {
+        for(gridDivisionIndex in 0..GridDimension)  {
+            val strokeWidth =
+                if(gridDivisionIndex in thickDividerIndexes)
+                    Stroke.DefaultMiter
+                else
+                    Stroke.HairlineWidth
+            val gridDivisionOffset = gridDivisionIndex * GridDivisionSize
+            drawLine(
+                brush = Brush.linearGradient(colors = themeColors),
+                start = Offset(gridDivisionOffset, 0f),
+                end = Offset(gridDivisionOffset, GridSize),
+                strokeWidth = strokeWidth
+            )
+            drawLine(
+                brush = Brush.linearGradient(colors = themeColors),
+                start = Offset(0f, gridDivisionOffset),
+                end = Offset(GridSize, gridDivisionOffset),
+                strokeWidth = strokeWidth
+            )
+
         }
     }
 }
