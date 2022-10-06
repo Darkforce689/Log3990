74b323224675b892025134beb71f3e38f42fc10d
XavierDupuis <xavier0978@hotmail.fr>
2022-10-06 15:05:05 -0400

    added multipliers + refactors

--- mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/BoardView.kt
+++ mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/BoardView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -13,14 +14,12 @@ import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.polyscrabbleclient.game.model.BoardDimension
-import com.example.polyscrabbleclient.game.model.BoardRange
-import com.example.polyscrabbleclient.game.model.RowChar
+import com.example.polyscrabbleclient.game.domain.MultiplierType
+import com.example.polyscrabbleclient.game.domain.Multipliers
+import com.example.polyscrabbleclient.game.model.*
 import com.example.polyscrabbleclient.game.viewmodels.BoardViewModel
 
 const val GridDimension = BoardDimension + 1
@@ -29,10 +28,10 @@ val GridPadding = 4.dp
 val GridSize = BoardSize - GridPadding.times(2)
 val GridDivisionSize = GridSize / GridDimension
 val HeaderRange = (BoardRange.first+1)..(BoardRange.last+1)
-val HeaderTextSize = BoardSize.div(30)
+val HeaderTextSize = BoardSize.div(GridDimension).div(1.8f)
 
 @Composable
-fun BoardView () {
+fun BoardView() {
     val viewModel: BoardViewModel = viewModel()
 
     val themeColors = listOf(MaterialTheme.colors.primary, MaterialTheme.colors.secondary, MaterialTheme.colors.secondary)
@@ -68,12 +67,12 @@ fun BoardView () {
     fun DrawScope.drawColumnHeader(
         gridDivisionIndex: Int,
         currentDivisionOffset: Float,
-        textPaint: NativePaint,
+        headerTextPaint: NativePaint,
         divisionCenterOffset: Float
     ) {
         val horizontalTextOffset =
             currentDivisionOffset +
-            divisionCenterOffset - 
+            divisionCenterOffset -
             // TODO : WARNING -> TWEAK
             (gridDivisionIndex / 10) * (HeaderTextSize.toPx()/4)
         drawIntoCanvas {
@@ -82,7 +81,7 @@ fun BoardView () {
                 horizontalTextOffset,
                 // TODO : WARNING -> TWEAK
                 2.4f * divisionCenterOffset,
-                textPaint
+                headerTextPaint
             )
         }
     }
@@ -90,7 +89,7 @@ fun BoardView () {
     fun DrawScope.drawRowHeader(
         gridDivisionIndex: Int,
         currentDivisionOffset: Float,
-        textPaint: NativePaint,
+        headerTextPaint: NativePaint,
         divisionCenterOffset: Float
     ) {
         val rowCharIndex = gridDivisionIndex - 2
@@ -100,12 +99,12 @@ fun BoardView () {
                 rowHeaderChar,
                 divisionCenterOffset,
                 currentDivisionOffset - divisionCenterOffset,
-                textPaint
+                headerTextPaint
             )
         }
     }
 
-    fun DrawScope.drawBackground() {
+    fun DrawScope.drawGridBackground() {
         drawRect(
             brush = Brush.linearGradient(colors = themeColors),
             topLeft = Offset(0f, 0f),
@@ -114,8 +113,12 @@ fun BoardView () {
         )
     }
 
-    fun DrawScope.drawGrid(textPaint: NativePaint, divisionCenterOffset: Float) {
-        drawBackground()
+    fun DrawScope.drawGridLayout(headerTextPaint: NativePaint) {
+        // TODO : WARNING -> TWEAK
+        val divisionCenterOffset = 0.3f * GridDivisionSize.toPx();
+
+        drawGridBackground()
+
         for (gridDivisionIndex in 0..GridDimension) {
             val strokeWidth =
                 if (gridDivisionIndex in thickDividerIndexes)
@@ -127,11 +130,20 @@ fun BoardView () {
             drawColumnDivider(currentDivisionOffset, strokeWidth)
             drawRowDivider(currentDivisionOffset, strokeWidth)
             if (gridDivisionIndex in BoardRange) {
-                drawColumnHeader(gridDivisionIndex, currentDivisionOffset, textPaint, divisionCenterOffset)
+                drawColumnHeader(gridDivisionIndex, currentDivisionOffset, headerTextPaint, divisionCenterOffset)
             }
             if (gridDivisionIndex in HeaderRange) {
                 drawRowHeader(gridDivisionIndex, currentDivisionOffset, textPaint, divisionCenterOffset)
             }
+        }
+    }
+
+    fun DrawScope.drawMultipliers() {
+        for (multiplier in Multipliers) {
+            val color =
+                if (multiplier.type === MultiplierType.Letter) themeColors[0] else themeColors[1]
+            val alpha = if (multiplier.value == 3) 0.7f else 0.2f
+            drawTileBackground(color, multiplier.column, multiplier.row.ordinal + 1, alpha)
         }
     }
 
@@ -140,18 +152,16 @@ fun BoardView () {
             .size(BoardSize)
             .padding(GridPadding)
     ) {
-
-
-        val textPaint = Paint().asFrameworkPaint().apply {
+        val headerTextPaint = Paint().asFrameworkPaint().apply {
             isAntiAlias = true
             textSize = HeaderTextSize.toPx()
             color = rowCharsColor.toArgb()
             typeface = Typeface.create(Typeface.MONOSPACE, Typeface.BOLD)
         }
 
-        // TODO : WARNING -> TWEAK
-        val divisionCenterOffset = 0.3f * GridDivisionSize.toPx();
-        drawGrid(textPaint, divisionCenterOffset)
+        drawMultipliers()
+        // WARNING -> drawGrid should be called after all others
+        drawGridLayout(headerTextPaint)
     }
 }
 
