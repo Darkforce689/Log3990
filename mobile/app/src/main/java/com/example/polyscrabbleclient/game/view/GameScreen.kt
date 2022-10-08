3cd393e0a907bba2563e1c41c2f5329c6220d9fb
XavierDupuis <xavier0978@hotmail.fr>
2022-10-07 22:40:02 -0400

    wip gamescreen drag and drop handling

--- mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/GameScreen.kt
+++ mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/GameScreen.kt
@@ -1,26 +1,70 @@
 package com.example.polyscrabbleclient.game.view
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.example.polyscrabbleclient.game.view.draganddrop.DraggedShadow
 import com.example.polyscrabbleclient.game.viewmodels.GameViewModel
+
+// From https://github.com/microsoft/surface-duo-compose-sdk/blob/main/DragAndDrop/library/src/main/java/com/microsoft/device/dualscreen/draganddrop/DragContainer.kt
+class DragState {
+    var isDragging: Boolean by mutableStateOf(false)
+    var dragPosition by mutableStateOf(Offset.Zero)
+    var dragOffset by mutableStateOf(Offset.Zero)
+    var draggableContent by mutableStateOf<(@Composable () -> Unit)?>(null)
+    var dragData by mutableStateOf<DragData?>(null)
+}
+
+// From https://github.com/microsoft/surface-duo-compose-sdk/blob/main/DragAndDrop/library/src/main/java/com/microsoft/device/dualscreen/draganddrop/DragData.kt
+class DragData(
+    val type: MimeType = MimeType.TEXT_PLAIN,
+    val data: Any? = null
+)
+
+// From https://github.com/microsoft/surface-duo-compose-sdk/blob/main/DragAndDrop/library/src/main/java/com/microsoft/device/dualscreen/draganddrop/MimeType.kt
+enum class MimeType(val value: String) {
+    IMAGE_JPEG("image/jpeg"),
+    TEXT_PLAIN("text/plain"),
+    UNKNOWN_TYPE("unknown")
+}
 
 @Composable
 fun GameScreen(navController: NavController?, gameViewModel: GameViewModel?) {
+    val dragState = DragState()
+    var dragText by remember { mutableStateOf<String?>(null) }
+    var dragImage by remember { mutableStateOf<Painter?>(null) }
+    val updateDragText: (String?) -> Unit = { newValue -> dragText = newValue }
+    val updateDragImage: (Painter?) -> Unit = { newValue -> dragImage = newValue }
+    var isDroppingItem by remember { mutableStateOf(false) }
+    var isItemInBounds by remember { mutableStateOf(false) }
+    val dragData by remember { mutableStateOf<DragData>(DragData()) }
+
     Column {
         Box {
-            BoardView()
+            BoardView(
+                dragState = dragState,
+                onDrag = { inBounds, isDragging ->
+                    isDroppingItem = isDragging
+                    isItemInBounds = inBounds
+                },
+            )
         }
         Box {
-            LetterRackView()
+            LetterRackView(dragData, dragState)
         }
+        DraggedShadow(
+            dragState = dragState
+        )
     }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_C)
+@Composable
+fun GameScreenPreview() {
+    GameScreen(null, null)
 }
