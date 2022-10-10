18fd44f0526bdcc179d730f09b3ef6be639fae14
XavierDupuis <xavier0978@hotmail.fr>
2022-10-10 14:07:39 -0400

    gamescreen layout

--- mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/GameScreen.kt
+++ mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/GameScreen.kt
@@ -1,12 +1,14 @@
 package com.example.polyscrabbleclient.game.view
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.example.polyscrabbleclient.game.domain.TileCreator
 import com.example.polyscrabbleclient.game.view.draganddrop.DragState
 import com.example.polyscrabbleclient.game.viewmodels.GameViewModel
 
@@ -16,15 +18,57 @@ fun GameScreen(navController: NavController?, gameViewModel: GameViewModel?) {
     // (https://developer.android.com/jetpack/compose/compositionlocal)
     val dragState = DragState()
 
+    EvenlySpacedRowContainer {
+        EvenlySpacedSubColumn {
+            Box {
+                // TODO : PLAYER INFOS
+                Text("PLAYERS INFOS")
+            }
+            Box {
+                // TODO : GAME INFOS
+                Text("GAME INFOS")
+            }
+            Box {
+                // TODO : GAME ACTIONS
+                Text("GAME ACTIONS")
+            }
+        }
+        EvenlySpacedSubColumn {
+            Box {
+                BoardView(dragState)
+            }
+            Box {
+                LetterRackView(dragState)
+            }
+        }
+        EvenlySpacedSubColumn {
+            Box {
+                // TODO : RIGHT PANEL
+                Text("RIGHT PANEL")
+            }
+        }
+    }
+}
+
+@Composable
+fun EvenlySpacedSubColumn(content: @Composable ColumnScope.() -> Unit) {
     Column (
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        Box {
-            BoardView(dragState)
-        }
-        Box {
-            LetterRackView(dragState)
-        }
+        content()
+    }
+}
+
+@Composable
+fun EvenlySpacedRowContainer(content: @Composable RowScope.() -> Unit) {
+    Row (
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        content()
     }
 }
 
