f7a283781740f95165ab6bee7ad1b46d54aac1d0
dasadou <65833938+dasadou@users.noreply.github.com>
2022-10-13 00:56:03 -0400

    UI creer , rejoindre partie et attendre partie

--- client/src/app/components/modals/join-online-game/join-online-game.component.ts
+++ client/src/app/components/modals/join-online-game/join-online-game.component.ts
@@ -1,21 +1,19 @@
-import { AfterContentChecked, ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, ValidatorFn, Validators } from '@angular/forms';
+import { AfterContentChecked, ChangeDetectorRef, Component, Inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ErrorDialogComponent } from '@app/components/modals/error-dialog/error-dialog.component';
-import { MAX_NAME_LENGTH, MIN_NAME_LENGTH } from '@app/game-logic/constants';
+import { AccountService } from '@app/services/account.service';
 import { GameLauncherService } from '@app/socket-handler/game-launcher/game-laucher';
 import { OnlineGameSettings } from '@app/socket-handler/interfaces/game-settings-multi.interface';
 import { NewOnlineGameSocketHandler } from '@app/socket-handler/new-online-game-socket-handler/new-online-game-socket-handler.service';
-const NO_WHITE_SPACE_RGX = /^\S*$/;
 @Component({
     selector: 'app-join-online-game',
     templateUrl: './join-online-game.component.html',
     styleUrls: ['./join-online-game.component.scss'],
 })
 // TODO GL3A22107-32 : No form needed
-export class JoinOnlineGameComponent implements AfterContentChecked, OnInit {
+export class JoinOnlineGameComponent implements AfterContentChecked {
     myName: FormControl;
-    private playerNames: string[];
 
     constructor(
         @Inject(MAT_DIALOG_DATA) public data: OnlineGameSettings,
@@ -24,18 +22,8 @@ export class JoinOnlineGameComponent implements AfterContentChecked, OnInit {
         private cdref: ChangeDetectorRef,
         private socketHandler: NewOnlineGameSocketHandler,
         private gameLaucherService: GameLauncherService,
+        private accountService: AccountService,
     ) {}
-
-    ngOnInit() {
-        this.playerNames = this.data.playerNames;
-        this.myName = new FormControl('', [
-            Validators.required,
-            Validators.minLength(MIN_NAME_LENGTH),
-            Validators.maxLength(MAX_NAME_LENGTH),
-            Validators.pattern(NO_WHITE_SPACE_RGX),
-            this.forbiddenNameValidator(),
-        ]);
-    }
 
     ngAfterContentChecked() {
         this.cdref.detectChanges();
@@ -47,25 +35,15 @@ export class JoinOnlineGameComponent implements AfterContentChecked, OnInit {
     }
 
     sendParameter(): void {
-        this.dialogRef.close(this.myName.value);
-        this.socketHandler.joinPendingGame(this.data.id, this.myName.value);
+        this.dialogRef.close(this.accountService.account?.name);
+        this.socketHandler.joinPendingGame(this.data.id);
         this.socketHandler.error$.subscribe((error: string) => {
             if (error) {
                 this.dialog.open(ErrorDialogComponent, { disableClose: true, autoFocus: true, data: error });
             }
         });
 
-        this.gameLaucherService.waitForOnlineGameStart(this.myName.value);
-    }
-
-    private forbiddenNameValidator(): ValidatorFn {
-        return (control: AbstractControl): { [key: string]: unknown } | null =>
-            // TODO GL3A22107-32 : FIX NAME VALIDATOR (next line is faulty)
-            control.value !== this.playerNames ? null : { forbidden: control.value };
-    }
-
-    get valid() {
-        return this.myName.valid;
+        this.gameLaucherService.waitForOnlineGameStart();
     }
 
     get randomBonusType() {
--- client/src/app/components/modals/new-online-game-form/new-online-game-form.component.ts
+++ client/src/app/components/modals/new-online-game-form/new-online-game-form.component.ts
@@ -2,16 +2,17 @@ import { AfterContentChecked, ChangeDetectorRef, Component, Inject } from '@angu
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {
+    DEFAULT_NUMBER_OF_PLAYERS,
     DEFAULT_TIME_PER_TURN,
-    MAX_NAME_LENGTH,
+    MAX_NUMBER_OF_PLAYERS,
     MAX_TIME_PER_TURN,
-    MIN_NAME_LENGTH,
+    MIN_NUMBER_OF_PLAYERS,
     MIN_TIME_PER_TURN,
-    STEP_TIME_PER_TURN,
+    STEP_TIME_PER_TURN
 } from '@app/game-logic/constants';
 import { OnlineGameSettingsUI } from '@app/socket-handler/interfaces/game-settings-multi.interface';
 
-const NO_WHITE_SPACE_RGX = /^\S*$/;
+// const NO_WHITE_SPACE_RGX = /^\S*$/;
 
 @Component({
     selector: 'app-new-online-game-form',
@@ -20,23 +21,24 @@ const NO_WHITE_SPACE_RGX = /^\S*$/;
 })
 export class NewOnlineGameFormComponent implements AfterContentChecked {
     onlineGameSettingsUIForm = new FormGroup({
-        playerName: new FormControl('', [
-            Validators.required,
-            Validators.minLength(MIN_NAME_LENGTH),
-            Validators.maxLength(MAX_NAME_LENGTH),
-            Validators.pattern(NO_WHITE_SPACE_RGX),
-        ]),
         timePerTurn: new FormControl(DEFAULT_TIME_PER_TURN, [
             Validators.required,
             Validators.min(MIN_TIME_PER_TURN),
             Validators.max(MAX_TIME_PER_TURN),
         ]),
         randomBonus: new FormControl(false, [Validators.required]),
+        numberOfPlayers: new FormControl(DEFAULT_NUMBER_OF_PLAYERS, [
+            Validators.required,
+            Validators.min(MIN_NUMBER_OF_PLAYERS),
+            Validators.max(MAX_NUMBER_OF_PLAYERS),
+        ]),
     });
 
     minTimePerTurn = MIN_TIME_PER_TURN;
     maxTimePerTurn = MAX_TIME_PER_TURN;
     stepTimePerTurn = STEP_TIME_PER_TURN;
+    minNumberOfPlayers = MIN_NUMBER_OF_PLAYERS;
+    maxNumberOfPlayers = MAX_NUMBER_OF_PLAYERS;
 
     constructor(
         @Inject(MAT_DIALOG_DATA) public data: OnlineGameSettingsUI,
@@ -62,7 +64,6 @@ export class NewOnlineGameFormComponent implements AfterContentChecked {
     cancel(): void {
         this.dialogRef.close();
         this.onlineGameSettingsUIForm.reset({
-            playerName: '',
             timePerTurn: DEFAULT_TIME_PER_TURN,
             randomBonus: false,
         });
