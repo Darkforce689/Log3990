a90b3976be2bf697bd0a077bc867be61549790e3
XavierDupuis <xavier0978@hotmail.fr>
2022-10-13 14:55:41 -0400

    missing import

--- mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/GameScreen.kt
+++ mobile/app/src/main/java/com/example/polyscrabbleclient/game/view/GameScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
