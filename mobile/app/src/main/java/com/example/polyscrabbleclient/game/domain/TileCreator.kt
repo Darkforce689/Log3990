bf59fab1e1a12152e3b8e1879702d04bafb9ad8c
XavierDupuis <xavier0978@hotmail.fr>
2022-10-13 21:13:53 -0400

    tilecreator object singleton

--- mobile/app/src/main/java/com/example/polyscrabbleclient/game/domain/TileCreator.kt
+++ mobile/app/src/main/java/com/example/polyscrabbleclient/game/domain/TileCreator.kt
@@ -1,9 +1,12 @@
 package com.example.polyscrabbleclient.game.domain
 
 import com.example.polyscrabbleclient.game.model.TileModel
+import com.example.polyscrabbleclient.game.sources.Tile
 
 
-class TileCreator {
+object TileCreator {
+    private val letterValues = arrayListOf(1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 10, 1, 2, 1, 1, 3, 8, 1, 1, 1, 1, 4, 10, 10, 10, 10)
+
     fun createTileFromLetter(letter: Char): TileModel {
         var points = 0;
         val letterIndex = letter.uppercaseChar().code - 'A'.code;
@@ -13,7 +16,14 @@ class TileCreator {
         return TileModel(letter, points)
     }
 
-    companion object{
-        val letterValues = arrayListOf(1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 10, 1, 2, 1, 1, 3, 8, 1, 1, 1, 1, 4, 10, 10, 10, 10)
+    fun createTileFromRawTile(tile: Tile): TileModel? {
+        val isNullTile =
+            tile.letterObject.char.length != 1 ||
+            tile.letterObject.char[0] == ' '
+        return if (isNullTile) {
+            null
+        } else {
+            createTileFromLetter(tile.letterObject.char[0])
+        }
     }
 }
