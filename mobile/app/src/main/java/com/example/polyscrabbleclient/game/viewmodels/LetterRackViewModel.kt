48ff8d7d9f366c2f071062965880533c8ef90870
XavierDupuis <xavier0978@hotmail.fr>
2022-10-13 21:14:07 -0400

    Update LetterRackViewModel.kt

--- mobile/app/src/main/java/com/example/polyscrabbleclient/game/viewmodels/LetterRackViewModel.kt
+++ mobile/app/src/main/java/com/example/polyscrabbleclient/game/viewmodels/LetterRackViewModel.kt
@@ -2,7 +2,7 @@ package com.example.polyscrabbleclient.game.viewmodels
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
-import com.example.polyscrabbleclient.game.domain.LetterCreator
+import com.example.polyscrabbleclient.game.domain.TileCreator
 import com.example.polyscrabbleclient.game.sources.GameRepository
 import com.example.polyscrabbleclient.game.sources.Player
 import com.example.polyscrabbleclient.game.view.draganddrop.DraggableContent
@@ -10,10 +10,9 @@ import com.example.polyscrabbleclient.game.view.draganddrop.DraggableContentType
 
 class LetterRackViewModel: ViewModel() {
     private val gameRepository = GameRepository
-    private val letterCreator = LetterCreator();
     private val player = Player("A")
     val tiles = mutableStateListOf (
-        *player.letters.map { letter -> letterCreator.createTileFromLetter(letter) }.toTypedArray()
+        *player.letters.map { letter -> TileCreator.createTileFromLetter(letter) }.toTypedArray()
     )
 
     fun removeTile(draggableContent: DraggableContent?) {
