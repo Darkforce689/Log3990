57826b396bba173bb527a6f633b761e7daf5da5d
XavierDupuis <xavier0978@hotmail.fr>
2022-10-17 18:15:16 -0400

    server NewGameSocketHandler adaptation (to be removed)
    waiting for !54

--- server/app/new-game/new-game-socket-handler/new-game-socket-handler.ts
+++ server/app/new-game/new-game-socket-handler/new-game-socket-handler.ts
@@ -16,6 +16,8 @@ const gameJoined = 'gameJoined';
 const gameStarted = 'gameStarted';
 const pendingGameId = 'pendingGameId';
 
+// TODO : REMOVE (ONLY TEMPORARY WHILE WAITING FOR !54)
+let a = 1;
 export class NewGameSocketHandler {
     readonly ioServer: Server;
 
@@ -53,9 +55,10 @@ export class NewGameSocketHandler {
                 }
             });
 
-            socket.on(joinGame, (id: string, name: string) => {
+            // TODO : REVERT/ACCEPT OTHER CHANGES (ONLY TEMPORARY WHILE WAITING FOR !54)
+            socket.on(joinGame, (id: string, name?: string) => {
                 try {
-                    this.joinGame(id, name, this.getPendingGame(id), socket);
+                    this.joinGame(id, name ?? `PlayerMobile#${a++}`, this.getPendingGame(id), socket);
                     this.emitPendingGamesToAll();
                 } catch (error) {
                     ServerLogger.logError(error);
