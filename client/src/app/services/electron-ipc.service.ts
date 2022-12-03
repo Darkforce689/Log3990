ec687edf76986517f9401af876144d3ba1145bef
Gabriel <gabriel-1.paradis@polymtl.ca>
2022-12-02 21:17:54 -0500

    remove comments

--- client/src/app/services/electron-ipc.service.ts
+++ client/src/app/services/electron-ipc.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-// TODO RELEASE : UNCOMMENT FILE
-// import { ipcRenderer } from 'electron';
+import { ipcRenderer } from 'electron';
 import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
@@ -10,36 +9,36 @@ export class ElectronIpcService {
     externalWindow$ = new BehaviorSubject<boolean>(false);
     leaveGameConvo$ = new Subject<void>();
     joinGameConvo$ = new Subject<string>();
-    // ipcRenderer: typeof ipcRenderer;
-    // constructor() {
-    // this.ipcRenderer = ipcRenderer;
-    // this.ipcRenderer.on('open-chat-box', (event, args) => {
-    //     this.externalWindow$.next(args);
-    // });
-    // this.ipcRenderer.on('game-token', (event, args) => {
-    //     this.joinGameConvo$.next(args);
-    // });
-    // this.ipcRenderer.on('leave-game-convo', (event, args) => {
-    //     this.leaveGameConvo$.next();
-    // });
-    // }
+    ipcRenderer: typeof ipcRenderer;
+    constructor() {
+        this.ipcRenderer = ipcRenderer;
+        this.ipcRenderer.on('open-chat-box', (event, args) => {
+            this.externalWindow$.next(args);
+        });
+        this.ipcRenderer.on('game-token', (event, args) => {
+            this.joinGameConvo$.next(args);
+        });
+        this.ipcRenderer.on('leave-game-convo', (event, args) => {
+            this.leaveGameConvo$.next();
+        });
+    }
 
     leaveGameConvo() {
-        // this.ipcRenderer.send('leave-game-convo');
+        this.ipcRenderer.send('leave-game-convo');
     }
 
     openPage(route: string) {
-        // this.ipcRenderer.send('open-external-window', route);
+        this.ipcRenderer.send('open-external-window', route);
         // La fonction n'a pas a return, c'est pour la compilation
         return route;
     }
 
     closePage() {
-        // this.ipcRenderer.send('close-external-window');
+        this.ipcRenderer.send('close-external-window');
     }
 
     sendGameToken(gameToken: string) {
-        // this.ipcRenderer.send('game-token', gameToken);
+        this.ipcRenderer.send('game-token', gameToken);
         // La fonction n'a pas a return, c'est pour la compilation
         return gameToken;
     }
